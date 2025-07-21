3495d9d78585968e9ee63d7f058c3d6a54ab0f8d
warrengami <142363273+warrengami@users.noreply.github.com>
2025-07-20 21:18:51 -0400

    new ACCESS_CODE_HASH from environment variables

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = 3000;
 
 // Hashed version of "WARRENGAMI2025"
-const ACCESS_CODE_HASH = '$2b$10$w.a9.yR7G0kY2J3p7dC9Auv2eYmI7jU9k.g3x.v8B.T1f.L6o.C5m';
+const ACCESS_CODE_HASH = '$2b$10$ZJIiuZ7050sPOiwkUor4auJXWrn1gxf0bCY0.9dQQmL.DeDMzwvNW';
 // AFTER
 const JWT_SECRET = process.env.JWT_SECRET;
 
